40645e1975feba36a77e808cf27c573e0e6b7a83
Osnat Ackerman <osnatackerman0376@gmail.com>
2019-05-30 16:10:33 +0300

    picture

--- Exercise3/App_Start/RouteConfig.cs
+++ Exercise3/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace Exercise3
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("display", "display/{ip}/{port}",
+            defaults: new { controller = "First", action = "display" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "First", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ Exercise3/Controllers/FirstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercise3.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult display(string ip, int port)
+        {
+            return View();
+        }
+    }
+
+
+
+    
+}
